574904fa6df59b8fb64c0d5b09aa38bc0cbce5e1
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-10 00:57:23 +0200

    feat: Added BlazorRoom model

---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
